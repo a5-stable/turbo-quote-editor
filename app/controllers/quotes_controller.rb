81151b4d91de360793953ed20ca25db34c87f7de
a5-stable <sh07e1916@gmail.com>
2024-01-13 14:04:06 +0900

    enable to create new quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
   before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
   def index
-    @quotes = Quote.all
+    @quotes = Quote.ordered
   end
 
   def show
@@ -13,9 +13,12 @@ class QuotesController < ApplicationController
   end
 
   def create
-    quote = Quote.new(quote_params)
-    if quote.save
-      redirect_to quotes_path, notice: "Quote was successfully created."
+    @quote = Quote.new(quote_params)
+    if @quote.save
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
+        format.turbo_stream
+      end
     else
       render :new, status: :unprocessable_entity
     end
